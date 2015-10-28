e379d6ca1c2d5ebabb91fa67b5e37d4433d16987
Patrick Lower <devvortex@gmail.com>
2015-10-28 14:55:18 -0400

    Shim library for missing symbols in Samsung blobs
     * Provide missing symbol needed for Samsung RIL
     * rild binary and libril.so library needs to be replaced by ones
       from a lollipop ROM. The ones from kitkat will simply crash.
    
    Change-Id: I7e38243e7958e82831eac0a0d9cb3e2f46c6aaef

--- klte.mk
+++ klte.mk
@@ -113,6 +113,10 @@ PRODUCT_PACKAGES += \
     init.sec.boot.sh \
     ueventd.qcom.rc
 
+# Samsung symbols
+PRODUCT_PACKAGES += \
+    libsamsung_symbols
+
 # Thermal
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine-8974.conf:system/etc/thermal-engine-8974.conf
